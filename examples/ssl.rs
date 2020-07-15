b38cb01d21d68db16dc536a7a447e9607b02ffea
Alekos Filini <alekos.filini@gmail.com>
2020-07-15 12:04:19 +0200

    [wip] update examples

--- examples/ssl.rs
+++ examples/ssl.rs
@@ -3,7 +3,7 @@ extern crate electrum_client;
 use electrum_client::Client;
 
 fn main() {
-    let mut client = Client::new_ssl("electrum2.hodlister.co:50002", true).unwrap();
+    let client = Client::new_ssl("electrum2.hodlister.co:50002", true).unwrap();
     let res = client.server_features();
     println!("{:#?}", res);
 }
--- examples/tor.rs
+++ examples/tor.rs
@@ -6,12 +6,12 @@ fn main() {
     // NOTE: This assumes Tor is running localy, with an unauthenticated Socks5 listening at
     // localhost:9050
 
-    let mut client = Client::new_proxy("ozahtqwp25chjdjd.onion:50001", "127.0.0.1:9050").unwrap();
+    let client = Client::new_proxy("ozahtqwp25chjdjd.onion:50001", "127.0.0.1:9050").unwrap();
     let res = client.server_features();
     println!("{:#?}", res);
 
     // works both with onion v2/v3 (if your Tor supports them)
-    let mut client = Client::new_proxy(
+    let client = Client::new_proxy(
         "v7gtzf7nua6hdmb2wtqaqioqmesdb4xrlly4zwr7bvayxv2bpg665pqd.onion:50001",
         "127.0.0.1:9050",
     )
