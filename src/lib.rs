02c72cc088f08c4a9d45d2aaf52244bfcbdef728
valued mammal <valuedmammal@protonmail.com>
2025-11-03 10:25:25 -0500

    build!: Remove unused `minimal` feature

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 //! plaintext connections over a socks proxy, useful for Onion servers. Using different features,
 //! the SSL implementation can be removed or replaced with [`openssl`](https://docs.rs/openssl).
 //!
-//! A `minimal` configuration is also provided, which only includes the plaintext TCP client.
+//! For a minimal configuration the library can be built with `--no-default-features`, which only includes the plaintext TCP client.
 //!
 //! # Example
 //!
